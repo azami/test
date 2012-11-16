e9ff9ecc878d3edb10cdf217c7045795e195b7c6
az <maki19830926@gmail.com>
2012-11-17 03:33:53 +0900

    so sleeply

--- flask_auth.py
+++ flask_auth.py
@@ -1,26 +1,24 @@
 #-*- coding: utf-8 -*-
 from oauth2client.client import OAuth2WebServerFlow
-import config
+import config as conf
 from flask import Flask, request, redirect
 
 
 app = Flask(__name__)
-app.debug = True
 app.secret = 'test'
 
+flow = OAuth2WebServerFlow(client_id=conf.Config.CLIENT_ID,
+                           client_secret=conf.Config.CLIENT_SECRET,
+                           scope=conf.Config.AUTH_SCOPE,
+                           redirect_uri=conf.Config.HOST + 'auth')
 
-def parse_token(token):
-    print token
-    return dict([x.split('=') for x in token.split('&')])
 
-
-
-def auth_google(conf):
+def auth_google():
     print 'hoge'
-    flow = OAuth2WebServerFlow(client_id=conf.CLIENT_ID,
-                               client_secret=conf.CLIENT_SECRET,
-                               scope=conf.AUTH_SCOPE,
-                               redirect_uri=conf.HOST + 'auth')
+    flow = OAuth2WebServerFlow(client_id=conf.Config.CLIENT_ID,
+                               client_secret=conf.Config.CLIENT_SECRET,
+                               scope=conf.Config.AUTH_SCOPE,
+                               redirect_uri=conf.Config.HOST + 'auth')
     auth_uri = flow.step1_get_authorize_url()
     print auth_uri
     return auth_uri
@@ -28,15 +26,15 @@ def auth_google(conf):
 
 @app.route('/auth')
 def auth_return():
-    credentials = flow.step2_exchange(code)
+    credentials = flow.step2_exchange(request.args.get('code', ''))
     print credintials
     return 'test'
 
 
 @app.route('/')
 def auth():
-    return redirect(auth_google(config.Config))
+    return redirect(auth_google())
 
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run()
