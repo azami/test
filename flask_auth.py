f20d33ffeff06b5137575f9998cbd74513a2db33
az <maki19830926@gmail.com>
2012-11-17 13:47:09 +0900

    use google identify

--- flask_auth.py
+++ flask_auth.py
@@ -1,40 +1,22 @@
 #-*- coding: utf-8 -*-
 from oauth2client.client import OAuth2WebServerFlow
-import config as conf
-from flask import Flask, request, redirect
+from config import Config
+from flask import Flask, request, redirect, render_template
 
 
 app = Flask(__name__)
-app.secret = 'test'
-
-flow = OAuth2WebServerFlow(client_id=conf.Config.CLIENT_ID,
-                           client_secret=conf.Config.CLIENT_SECRET,
-                           scope=conf.Config.AUTH_SCOPE,
-                           redirect_uri=conf.Config.HOST + 'auth')
-
-
-def auth_google():
-    print 'hoge'
-    flow = OAuth2WebServerFlow(client_id=conf.Config.CLIENT_ID,
-                               client_secret=conf.Config.CLIENT_SECRET,
-                               scope=conf.Config.AUTH_SCOPE,
-                               redirect_uri=conf.Config.HOST + 'auth')
-    auth_uri = flow.step1_get_authorize_url()
-    print auth_uri
-    return auth_uri
-
-
-@app.route('/auth')
-def auth_return():
-    credentials = flow.step2_exchange(request.args.get('code', ''))
-    print credintials
-    return 'test'
 
 
 @app.route('/')
 def auth():
-    return redirect(auth_google())
+    print Config.dev_key
+    return render_template('base.htm', dev_key=Config.dev_key,
+                           callback=Config.host + '/callback')
 
+
+@app.route('/callback')
+def callback():
+    return request.args
 
 if __name__ == '__main__':
     app.run()
